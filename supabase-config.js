2cbe4e0b4d390d0719f80c757ab8f54f21108673
benhviencaungang <admin@benhviencaungang.com>
2026-08-22 16:09:50 +0700

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,6 @@
-// Thông tin kết nối từ tài khoản Supabase của bạn
-const SUPABASE_URL = 'https://ciqophqzbmkplgkdccwd.supabase.co';
+// Thông tin kết nối mới từ máy chủ vật lý qua Cloudflare Tunnel
+const SUPABASE_URL = 'https://merely-cheque-appeals-sunset.trycloudflare.com';
 const SUPABASE_ANON_KEY = 'sb_publishable_ublUNkIvVWK0ETY2h52pdA_CpbUOFoI';
 
-// Khởi tạo client Supabase (Đổi tên thành _supabase để tránh xung đột với thư viện gốc)
+// Khởi tạo client Supabase
 const _supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
